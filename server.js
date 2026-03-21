eb56e68490a746e49c000eb52c25a948dc465443
Frank Kang <fronkdetonk@gmail.com>
2026-03-21 14:10:01 -0500

    Create backend web server for website frontend using Express.js and shift website to Single-Page Application

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("frontend"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "home.html"));
+});
+
+app.get("/*splat", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "home.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
