4e1cd00160efc89eaf22a759505f48fb9fec1900
Frank Kang <fronkdetonk@gmail.com>
2026-03-23 17:39:37 -0500

    Add separate back buttons for task and reminder adding.

--- frontend/static/views/Home.js
+++ frontend/static/views/Home.js
@@ -13,18 +13,45 @@ export default class extends AbstractView {
 
     init () {
         const chatboxSections = document.querySelectorAll(".chatbox-section");
-
+        const taskSection = document.getElementById("add-task");
+        const reminderSection = document.getElementById("add-reminder");
+        const taskBackButton = document.getElementById("back-task");
+        const reminderBackButton = document.getElementById("back-reminder");
         chatboxSections.forEach(chatboxSection => {
             chatboxSection.addEventListener("click", () => {
                 console.log("Click");
                 chatboxSections.forEach(s => {
                     if (s === chatboxSection) {
-                        s.classList.add("expanded");
+                        s.classList.replace("split","expanded");
                     } else {
-                        s.classList.add("hidden");
+                        s.classList.add("split","hidden");
                     }
                 });
+                setTimeout(() => {
+                    console.log("Made button visible");
+                    if (taskSection.classList.contains("expanded")) {
+                        taskBackButton.classList.add("visible");
+                    } else if (reminderSection.classList.contains("expanded")) {
+                        reminderBackButton.classList.add("visible");
+                    }
+                }, 300);
             });
         });
+        
+        const backButtons = document.querySelectorAll(".back-button");
+        backButtons.forEach(backButton => {
+            backButton.addEventListener("click", () => {
+                console.log("Back button click");
+                chatboxSections.forEach(s => {
+                    setTimeout(() => {
+                        s.classList.remove("expanded", "hidden");
+                        s.classList.add("split");
+                        backButton.classList.remove("visible");
+                    },300);
+                        
+                });
+            });
+        })
+            
     }
 }
