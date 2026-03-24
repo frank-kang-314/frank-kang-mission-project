4f316203c2a31f5e5c0c19379f6dca5e592c877f
Frank Kang <fronkdetonk@gmail.com>
2026-03-24 14:47:57 -0500

    Change styles for task and reminder input fields.

--- frontend/static/views/Home.js
+++ frontend/static/views/Home.js
@@ -20,42 +20,58 @@ export default class extends AbstractView {
         })("/static/css/home.css");
         
         const chatboxSections = document.querySelectorAll(".chatbox-section");
+
         const taskSection = document.getElementById("add-task");
-        const reminderSection = document.getElementById("add-reminder");
         const taskBackButton = document.getElementById("back-task");
+        const taskInputField = document.getElementById("new-task")
+        
+        const reminderSection = document.getElementById("add-reminder");
         const reminderBackButton = document.getElementById("back-reminder");
+        const reminderInputField = document.getElementById("new-reminder")
+
         chatboxSections.forEach(chatboxSection => {
+            // Interactivity for task and reminder boxes
             chatboxSection.addEventListener("click", () => {
-                console.log("Click");
+                
+                // If section is clicked, expand it and collapse the other section
                 chatboxSections.forEach(s => {
                     if (s === chatboxSection) {
                         s.classList.replace("split","expanded");
                     } else {
-                        s.classList.add("split","hidden");
+                        s.classList.replace("split","hidden");
                     }
                 });
+
+                // Show the back button and the input field
                 setTimeout(() => {
                     console.log("Made button visible");
                     if (taskSection.classList.contains("expanded")) {
                         taskBackButton.classList.add("visible");
+                        taskInputField.classList.add("visible")
                     } else if (reminderSection.classList.contains("expanded")) {
                         reminderBackButton.classList.add("visible");
+                        reminderInputField.classList.add("visible") 
                     }
                 }, 300);
             });
         });
         
+        // Returning to split view
         const backButtons = document.querySelectorAll(".back-button");
+        const inputFields = document.querySelectorAll(".add-item-field");
         backButtons.forEach(backButton => {
             backButton.addEventListener("click", () => {
-                console.log("Back button click");
+                // Runs when any of the two back buttons are clicked
                 chatboxSections.forEach(s => {
                     setTimeout(() => {
                         s.classList.remove("expanded", "hidden");
                         s.classList.add("split");
                         backButton.classList.remove("visible");
                     },300);
-                        
+                });
+                
+                inputFields.forEach(inputField => {
+                    inputField.classList.remove("visible");
                 });
             });
         })
