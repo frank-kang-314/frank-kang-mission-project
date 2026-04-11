2b5e06ce01fbfc24fb7e05a91c13a882d21a0f38
Frank Kang <fronkdetonk@gmail.com>
2026-04-10 22:37:58 -0500

    Fix more typos

--- frontend/static/views/Calendar.js
+++ frontend/static/views/Calendar.js
@@ -1,5 +1,5 @@
 import AbstractView from "./AbstractView.js";
-import { html, css } from "../js/config.js";
+import { html, css } from "../js/paths.js";
 
 export default class extends AbstractView {
     constructor() {
@@ -8,7 +8,7 @@ export default class extends AbstractView {
     }
 
     async getHtml() {
-        const response = await fetch(html("home.html"));
+        const response = await fetch(html("calendar.html"));
         return await response.text();
     }
 
--- frontend/static/views/Home.js
+++ frontend/static/views/Home.js
@@ -1,5 +1,5 @@
 import AbstractView from "./AbstractView.js";
-import { html, css } from "../js/config.js";
+import { html, css } from "../js/paths.js";
 
 export default class extends AbstractView {
     constructor() {
